1084678b64755dd4c49bca041e9bff7a52e18204
Anne-Sophie Vandendooren <anne.vandendooren@gmail.com>
2019-09-24 17:15:44 +0200

    clean files

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './Stylesheets/App.css';
-import {Route, Switch} from "react-router-dom";
+import {Route} from "react-router-dom";
 import Streams from "./components/Streams"
-import Game from "./components/Game"
 
 
 function App() {
   return (
-    <div className="App">
-      <Switch>
+    <div>
         <Route exact path="/" component={Streams} /> 
-        <Route path="/game/:id" component={Game} /> 
-      </Switch>
-
     </div>
   );
 }
--- src/components/Streams.js
+++ src/components/Streams.js
@@ -1,151 +1,142 @@
 import React, { Component } from 'react'
 import api from "../Api"
-import {Link} from "react-router-dom"
 import '../Stylesheets/Streams.css';
 
 
 export default class Streams extends Component {
   
 
-    constructor(){
-      super()
-        this.state = {  
-          streams : [], 
-          search : [],
-        }
+  constructor(){
+    super()
+      this.state = {  
+        streams : [], 
+    }
+  }
+
+
+  componentDidMount() {
+    api.get("https://api.twitch.tv/kraken/streams/")
+    .then(response => {
+      this.setState({streams: response.data.streams})
+    })
+
+  }
+
+      
+  sortByViews = () => {
+    const copiedDisplay = [].concat(this.state.streams)
+    
+    copiedDisplay.sort(function(a, b) {
+      var streamA = a.channel.views
+      var streamB = b.channel.views
+      if (streamA < streamB) {
+        return 1;
+      }
+      if (streamA > streamB) {
+        return -1;
       }
     
+      return 0;
+    });
+    this.setState({
+      streams: copiedDisplay
+    })
+  }
 
-    componentDidMount() {
-      api.get("https://api.twitch.tv/kraken/streams/")
-      .then(response => {
-        this.setState({streams: response.data.streams})
-      })
+  sortByFollowers = () => {
+    const copiedDisplay = [].concat(this.state.streams)
+    
+    copiedDisplay.sort(function(a, b) {
+      var streamA = a.channel.followers
+      var streamB = b.channel.followers
+      if (streamA < streamB) {
+        return 1;
+      }
+      if (streamA > streamB) {
+        return -1;
+      }
+    
+      return 0;
+    });
+    this.setState({
+      streams: copiedDisplay
+    })
+  }
 
-    }
+  sortByGameName = () => {
+    const copiedDisplay = [].concat(this.state.streams)
+    
+    copiedDisplay.sort(function(a, b) {
+      var streamA = a.game
+      var streamB = b.game
+      if (streamA < streamB) {
+        return -1;
+      }
+      if (streamA > streamB) {
+        return 1;
+      }
+    
+      // names must be equal
+      return 0;
+    });
+    this.setState({
+      streams: copiedDisplay
+    })
+  }
 
-       
-    sortByViews = () => {
-      const copiedDisplay = [].concat(this.state.streams)
-      
-      copiedDisplay.sort(function(a, b) {
-        var streamA = a.channel.views
-        var streamB = b.channel.views
-        if (streamA < streamB) {
-          return 1;
-        }
-        if (streamA > streamB) {
-          return -1;
-        }
-      
-        return 0;
-      });
-      this.setState({
-        streams: copiedDisplay
-      })
-    }
+  search = (event)=> {
+    let searchTerm = event.target.value.toUpperCase();
+    let searchedGame = this.state.streams.filter((streams)=> (
+      streams.channel.game.toUpperCase().indexOf(searchTerm) >= 0
+    ))  
+    this.setState({streams: searchedGame})
+  }
 
-    sortByFollowers = () => {
-      const copiedDisplay = [].concat(this.state.streams)
-      
-      copiedDisplay.sort(function(a, b) {
-        var streamA = a.channel.followers
-        var streamB = b.channel.followers
-        if (streamA < streamB) {
-          return 1;
-        }
-        if (streamA > streamB) {
-          return -1;
-        }
-      
-        return 0;
-      });
-      this.setState({
-        streams: copiedDisplay
-      })
-    }
-
-    sortByGameName = () => {
-      const copiedDisplay = [].concat(this.state.streams)
-      
-      copiedDisplay.sort(function(a, b) {
-        var streamA = a.game
-        var streamB = b.game
-        if (streamA < streamB) {
-          return -1;
-        }
-        if (streamA > streamB) {
-          return 1;
-        }
-      
-        // names must be equal
-        return 0;
-      });
-      this.setState({
-        streams: copiedDisplay
-      })
-    }
-
-    search = (event)=> {
-      let searchTerm = event.target.value.toUpperCase();
-      let searchedGame = this.state.streams.filter((streams)=> (
-        streams.channel.game.toUpperCase().indexOf(searchTerm) >= 0
-      ))  
-      this.setState({streams: searchedGame})
-    }
-
-    render() {
-      return (
-        <div>
-          <div>
-            <h1>List of streams</h1>
-              <input onChange={this.search} placeholder="Search for a Game" type="text"/>
-
-                <div className="Button-box">  
-                  <button className="Button" onClick={this.sortByGameName}>sort by Game</button>
-                  <button className="Button" onClick={this.sortByViews}>sort by views</button>
-                  <button className="Button" onClick={this.sortByFollowers}>sort by followers</button>
-                </div>
-          </div>
-           
-          <div className="Stream-container">
-            {this.state.streams ?
-            <>
-            {this.state.streams.map((streamsArray) => {
-              return (
-                <div className="Stream-box">
-                  <Link to={`/game/${streamsArray._id}`} >
-                    <h2>{streamsArray.game}</h2>
-                    <table>
-                      <thead>
-                          <tr>
-                              <th colspan="2">{streamsArray.channel.display_name}</th>
-                          </tr>
-                      </thead>
-                      <tbody>
-                          <tr>
-                              <td>Views</td>
-                              <td>{streamsArray.channel.views}</td>
-                          </tr>
-                          
-                          <tr>
-                              <td>Followers</td>
-                              <td>{streamsArray.channel.followers}</td>
-                          </tr>
-                      </tbody>
-                      
-                   </table>
-                   <img className="Stream-image" src={streamsArray.preview.medium} alt="SreamImage"/>
-
-          
-                  </Link> 
-                </div>
-              )
-            })}
-            </>
-            : null}
+  render() {
+    return (
+      <div>
+        <div className="Header">
+          <h1>LIST OF STREAMS</h1>
+          <input onChange={this.search} placeholder="Search for a Game" type="text"/>
+          <div className="Button-box">  
+            <button className="Button" onClick={this.sortByGameName}>sort by Game</button>
+            <button className="Button" onClick={this.sortByViews}>sort by views</button>
+            <button className="Button" onClick={this.sortByFollowers}>sort by followers</button>
           </div>
         </div>
-      )
+          
+        <div className="Stream-container">
+          {this.state.streams ?
+          <>
+          {this.state.streams.map((streamsArray) => {
+            return (
+              <div className="Stream-box">
+                  <h2>{streamsArray.game}</h2>
+                  <table>
+                    <thead>
+                        <tr>
+                            <th colspan="2">{streamsArray.channel.display_name}</th>
+                        </tr>
+                    </thead>
+                    <tbody>              
+                        <tr>
+                          <td>Views</td>
+                          <td>{streamsArray.channel.views}</td>
+                        </tr>                        
+                        <tr>
+                            <td>Followers</td>
+                            <td>{streamsArray.channel.followers}</td>
+                        </tr>
+                    </tbody>                    
+                  </table>
+                  <img className="Stream-image" src={streamsArray.preview.medium} alt="SreamImage"/>
+              </div>
+            )
+          })}
+          </>
+          : null}
+        </div>
+      </div>
+    )
   }
 }
