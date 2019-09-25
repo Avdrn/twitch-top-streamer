48f877d5a17fa9c047c353fc0874ec2bbcffa9cd
Anne-Sophie Vandendooren <anne.vandendooren@gmail.com>
2019-09-25 11:18:34 +0200

    responsive

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import {Route} from "react-router-dom";
 import Streams from "./components/Streams"
+import "./Stylesheets/App.css"
 
 
 function App() {
   return (
-    <div>
+    <div className="App">
         <Route exact path="/" component={Streams} /> 
     </div>
   );
